5580043b22be45e204af87e7bc0082f7b34d62e9
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-06-16 16:26:57 -0700

    clean up binary add and allow arbitrary number of inputs

--- binadd.js
+++ binadd.js
@@ -1,23 +1,15 @@
-function binadd (n, m) {
-  // normalize length
-  let a
-  let b
-  if (n.length === m.length) {
-    a = n
-    b = m
-  } else {
-    // a = longer, b = shorter
-    a = n.length > m.length ? n : m
-    b = m.length > n.length ? n : m
-    while (a.length !== b.length) {
-      b = '0' + b
-    }
+function normalizeLength (...nums) {
+  let max = 0
+  for (let n of nums) {
+    if (n.length > max) max = n.length
   }
+  return nums.map(n => '0'.repeat(max - n.length) + n)
+}
 
+function add (a, b) {
   let out = []
   let carry = 0
-  let i = a.length - 1
-  while (i >= 0) {
+  for (let i = a.length - 1; i >= 0; i--) {
     let next
     let nextCarry
     if (a[i] === '1' && b[i] === '1') {
@@ -28,20 +20,38 @@ function binadd (n, m) {
     } else if (a[i] === '1' || b[i] === '1') {
       next = 1
     }
-    if (carry) {
-      next = next + carry
-    }
+    next += carry
     carry = nextCarry || next > 1 ? 1 : 0
     next %= 2
     out.unshift(next ? '1' : '0')
-    console.log(a[i], b[i], { carry, out })
-    i--
   }
   if (carry) out.unshift('1')
   return out.join('')
 }
 
+function binadd (n, m) {
+  return add(...normalizeLength(n, m))
+}
+
+// built-ins
+function binadd2 (a, b) {
+  return (parseInt(a, 2) + parseInt(b, 2)).toString(2)
+}
+
+// can accept arbitrary number of inputs
+function binadd3 (...input) {
+  return input.reduce(binadd)
+}
+
+
+/***** testing */
+console.log(binadd3(
+  '101',      // 5
+  '1001',     // 9
+  '1010101'   // 85
+))            // 99 (1100011)
+
 console.log(binadd(
-  '110', // 6
-  '1010' // 10
-)) // 10000 (16)
+  '110',      // 6
+  '1010'      // 10
+))            // 16 (10000)
