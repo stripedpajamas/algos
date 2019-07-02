c97633a4029fc9a11f47e200648e51ac4578a158
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-02 08:35:45 -0700

    Rename misc class to PointProcessor

--- src/main/java/algos/PointProcessor.java
+++ src/main/java/algos/PointProcessor.java
@@ -2,7 +2,7 @@ package algos;
 
 import java.util.Arrays;
 
-public class Misc {
+public class PointProcessor {
 	public class PointDistance {
 		private final int[] point;
 		private final double distance;
@@ -12,6 +12,7 @@ public class Misc {
 			this.distance = distance;
 		}
 	}
+
 	/**
 	 * https://leetcode.com/problems/k-closest-points-to-origin/
 	 */
@@ -20,35 +21,28 @@ public class Misc {
 	}
 
 	/**
-	 * Frst map the points to their distance from the origin
-	 * then sort smallest to greatest
-	 * then slice the first k elements
+	 * Frst map the points to their distance from the origin then sort smallest to
+	 * greatest then slice the first k elements
+	 * 
 	 * @param points an array of x, y coordinates
-	 * @param k the limit for how many points to return
+	 * @param k      the limit for how many points to return
 	 * @return k closest points to the origin
 	 */
 	public int[][] mapSortLimit(final int[][] points, final int k) {
-		return Arrays.stream(points)
-			.map(p -> new PointDistance(p, calculateEuclidianDistanceFromOrigin(p)))
-			.sorted(this::byDistance)
-			.limit(k)
-			.map(pd -> pd.point)
-			.toArray(int[][]::new);
+		return Arrays.stream(points).map(p -> new PointDistance(p, calculateEuclidianDistanceFromOrigin(p)))
+				.sorted(this::byDistance).limit(k).map(pd -> pd.point).toArray(int[][]::new);
 	}
 
 	/**
-	 * Take the square root of the sum of the squares
-	 * of the differences of the coordinates. For distance from the 
-	 * origin, this effectively means sqrt(x^2 + y^2)
+	 * Take the square root of the sum of the squares of the differences of the
+	 * coordinates. For distance from the origin, this effectively means sqrt(x^2 +
+	 * y^2)
+	 * 
 	 * @param point [x, y]
 	 * @return the euclidian distance from the origin
 	 */
 	private double calculateEuclidianDistanceFromOrigin(final int[] point) {
-		return Math.sqrt(
-			Arrays.stream(point)
-				.map(p -> p * p)
-				.sum()
-		);
+		return Math.sqrt(Arrays.stream(point).map(p -> p * p).sum());
 	}
 
 	private int byDistance(final PointDistance a, final PointDistance b) {
--- src/test/java/algos/PointProcessorTest.java
+++ src/test/java/algos/PointProcessorTest.java
@@ -13,21 +13,19 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class MiscTest {
+public class PointProcessorTest {
 	@Parameters
 	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-			{createPointsFrom(1, 3, -2, 2), 1, createPointsFrom(-2, 2)},
-			{createPointsFrom(3, 3, 5, -1, -2, 4), 2, createPointsFrom(3, 3, -2, 4)}
-		});
+		return Arrays.asList(new Object[][] { { createPointsFrom(1, 3, -2, 2), 1, createPointsFrom(-2, 2) },
+				{ createPointsFrom(3, 3, 5, -1, -2, 4), 2, createPointsFrom(3, 3, -2, 4) } });
 	}
 
-	private final Misc misc = new Misc();
+	private final PointProcessor PointProcessor = new PointProcessor();
 	private final int[][] points;
 	private final int k;
 	private final int[][] expected;
 
-	public MiscTest(final int[][] points, final int k, final int[][] expected) {
+	public PointProcessorTest(final int[][] points, final int k, final int[][] expected) {
 		this.points = points;
 		this.k = k;
 		this.expected = expected;
@@ -35,7 +33,7 @@ public class MiscTest {
 
 	@Test
 	public void testKClosest_mapSortLimit() {
-		assertArrayEquals(expected, misc.kClosest(points, k));
+		assertArrayEquals(expected, PointProcessor.kClosest(points, k));
 	}
 
 	private static int[][] createPointsFrom(int... nums) {
@@ -46,7 +44,7 @@ public class MiscTest {
 		for (int i = 0; i < nums.length; i += 2) {
 			final int x = nums[i];
 			final int y = nums[i + 1];
-			output.add(new int[] {x, y});
+			output.add(new int[] { x, y });
 		}
 
 		return output.toArray(new int[0][0]);
