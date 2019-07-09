f92d57456cfdb9533b129c6741b84be89ece9dbe
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-08 17:57:01 -0700

    a tiny test for a tiny world

--- src/test/java/algos/ConnectedBinaryTreeTest.java
+++ src/test/java/algos/ConnectedBinaryTreeTest.java
@@ -1,10 +1,22 @@
 package algos;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class ConnectedBinaryTreeTest {
   @Test
-  public void testConnect() {
-    
+  public void testConnect_small() {
+    final BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+    bst.insert(Arrays.asList(2, 1, 3));
+
+    final ConnectedBinaryTree<Integer> actualTree = new ConnectedBinaryTree<>(bst.getRoot());
+
+    final ConnectedTreeNode<Integer> root = actualTree.getRoot();
+    assertEquals(null, root.getNext());
+    assertEquals(root.getRight(), root.getLeft().getNext());
+    assertEquals(null, root.getRight().getNext());
   }
 }
