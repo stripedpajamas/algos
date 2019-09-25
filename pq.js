1f52bc23b9530b51a70af9623a649fb948671212
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-09-24 20:12:10 -0700

    Implement top-down solution for pq

--- pq.js
+++ pq.js
@@ -13,11 +13,70 @@ const assert = require('assert')
  */
 
 function isInPq (str) {
-  return false
+  let parsed
+  try {
+    parsed = parse(str)
+  } catch (e) {
+    return false
+  }
+  return isAxiom(parsed) || isInPq(stringify(reduce(parsed)))
+}
+
+function parse (str) {
+  let [past, present, future] = ['', '', '']
+  let [havePast, havePresent] = [false, false, false]
+  for (let c of str) {
+    switch (c) {
+      case '-': {
+        if (!havePast) {
+          past += c
+          break
+        }
+        if (!havePresent) {
+          present += c
+          break
+        }
+        future += c
+        break
+      }
+      case 'p': {
+        havePast = true
+        break
+      }
+      case 'q': {
+        havePresent = true
+        break
+      }
+      default:
+        throw new Error('invalid input')
+    }
+  }
+  const parsed = [past, present, future]
+  if (!parsed.every(el => Boolean(el.length))) {
+    throw new Error('invalid input')
+  }
+  return parsed
+}
+
+function isAxiom ([past, present, future]) {
+  return past.length + 1 === future.length && present.length === 1
+}
+
+function reduce ([past, present, future]) {
+  return [past, present.slice(0, -1), future.slice(0, -1)]
+}
+
+function stringify([past, present, future]) {
+  return `${past}p${present}q${future}`
 }
 
 assert.ok(isInPq('-p-q--') === true)
+assert.ok(isInPq('---p-q--') === false)
+assert.ok(isInPq('---p---q--') === false)
 assert.ok(isInPq('--p---q-----') === true)
+assert.ok(isInPq('---p---q---') === false)
+assert.ok(isInPq('----p---q----') === false)
+assert.ok(isInPq('-----p---q-----') === false)
 assert.ok(isInPq('---p-q----') === true)
 assert.ok(isInPq('---p---q------') === true)
 assert.ok(isInPq('-p---q----') === true)
